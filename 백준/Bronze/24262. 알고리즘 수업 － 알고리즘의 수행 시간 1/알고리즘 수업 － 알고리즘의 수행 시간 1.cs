db2a472c09cc2cc945a1ec660e040c501900e0ac
baewhv <51593013+baewhv@users.noreply.github.com>
2025-12-22 22:41:33 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cs
@@ -0,0 +1,11 @@
+ using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(1);
+        Console.WriteLine(0);
+    }
+}
