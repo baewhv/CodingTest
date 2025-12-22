2df12fab4b8e39e83a0d66855bf45295266b4163
baewhv <51593013+baewhv@users.noreply.github.com>
2025-12-22 23:07:43 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(input);
+        Console.WriteLine(1);
+        
+    }
+}
