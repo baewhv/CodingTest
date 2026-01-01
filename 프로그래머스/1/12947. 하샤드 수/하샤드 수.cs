ca54391f9a7e1882eb09dd15b551ea98fcce573b
baewhv <51593013+baewhv@users.noreply.github.com>
2026-01-02 03:47:25 +0900

    [level 1] Title: 하샤드 수, Time: 0.20 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public bool solution(int x) {
+        int temp = x;
+        int sum = 0;
+        while(temp != 0)
+        {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return x % sum == 0;
+    }
+}
