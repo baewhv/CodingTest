8c6b7350a9d520112547ea5928ac21d6c96e1723
baewhv <51593013+baewhv@users.noreply.github.com>
2026-04-10 21:17:02 +0900

    [level 1] Title: 콜라 문제, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n >= a)
+        {
+            int temp = n / a * b;
+            answer += temp;
+            n = n % a + temp;
+        }
+        return answer;
+    }
+}
