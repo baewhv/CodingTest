1e058c1976fac75f18d1f930758c933a4de44efa
baewhv <51593013+baewhv@users.noreply.github.com>
2026-04-27 09:46:15 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string temp;
+    cin >> temp;
+    int n;
+    cin >> n;
+    
+    cout <<temp[n-1];
+    
+    return 0;
+}
